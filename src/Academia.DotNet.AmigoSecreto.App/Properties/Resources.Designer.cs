b648917a75a880d4a59f3e99d82baa8456c4ec74
PauloAlves8039 <pj38alves@gmail.com>
2023-11-17 21:42:00 -0300

    Atualizando arquivos de recursos

--- src/Academia.DotNet.AmigoSecreto.App/Properties/Resources.Designer.cs
+++ src/Academia.DotNet.AmigoSecreto.App/Properties/Resources.Designer.cs
@@ -129,5 +129,15 @@ namespace Academia.DotNet.AmigoSecreto.App.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap presente1 {
+            get {
+                object obj = ResourceManager.GetObject("presente1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
